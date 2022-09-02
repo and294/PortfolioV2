35a615d1440b9f4e0dbb25bb4651cccae2140ede
Glitch (glitch-blank-react) <none>
2022-09-02 09:59:53 +0000

    🔑🌗 Checkpoint ./src/pages/Home/index.jsx:32506642/329

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -22,7 +22,18 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
+position: relative;
+`;
 
+const Circle = styled.span`
+position: absolute;
+width: 500px;
+height: 500px;
+border: solid rgb(76, 96, 150) 10px;
+border-radius: 100%;
+z-index: 0;
+-webkit-box-shadow: 0px 0px 21px 10px #4C6096; 
+box-shadow: 0px 0px 21px 10px #4C6096;
 `;
 
 export default function Home() {
@@ -33,13 +44,13 @@ export default function Home() {
       <Landing>
        <Navbar />  
         <Name>
-       <h1 className="title" style={{fontSize: '126px', fontFamily: "SaintMonica"}}>ANDREA FERRARO</h1>
+       <h1 className="title" style={{fontSize: '126px', fontFamily: "SaintMonica", zIndex: "10"}}>ANDREA FERRARO</h1>
       <h2 style={{fontSize: '34px', fontFamily: "GT america"}}>Frontend Developer</h2>
           <span style={{width: "180px", height: "2px", backgroundColor: "rgb(76, 96, 150)", margin: "10px"}}></span>
       <p style={{fontSize: '14px'}}>
         Paris, France
       </p>
-          <span></span>
+          <Circle></Circle>
         </Name>
       </Landing>
       
