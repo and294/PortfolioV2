554b6e5bc0d5d2a599838f5d57c09e5465d99077
Glitch (glitch-blank-react) <none>
2022-09-02 10:12:01 +0000

    🏰⛩ Checkpoint ./src/pages/Home/index.jsx:32506642/1151

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -29,12 +29,27 @@ const Circle = styled.span`
 position: absolute;
 width: 500px;
 height: 500px;
-border: solid rgb(76, 96, 150) 10px;
+border: solid #4C6096 10px;
 border-radius: 100%;
 z-index: 0;
 -webkit-box-shadow: 0px 0px 21px 10px #4C6096; 
 box-shadow: 0px 0px 21px 10px #4C6096;
 `;
+
+const Resume = styled.button`
+width: 100px;
+height: 50px;
+color: white;
+font-family: GT america;
+background-color: #4C6096;
+border: none;
+border-radius: 25px;
+margin: 50px;
+position: absolute;
+bottom: 0;
+left: 0;
+`;
+
 
 export default function Home() {
  
@@ -45,13 +60,14 @@ export default function Home() {
        <Navbar />  
         <Name>
        <h1 className="title" style={{fontSize: '126px', fontFamily: "SaintMonica", zIndex: "10"}}>ANDREA FERRARO</h1>
-      <h2 style={{fontSize: '34px', fontFamily: "GT america"}}>Frontend Developer</h2>
+      <h2 style={{fontSize: '34px', fontFamily: "GT america", zIndex: "10"}}>Frontend Developer</h2>
           <span style={{width: "180px", height: "2px", backgroundColor: "rgb(76, 96, 150)", margin: "10px"}}></span>
-      <p style={{fontSize: '14px'}}>
+      <p style={{fontSize: '14px', zIndex: "10"}}>
         Paris, France
       </p>
-          <Circle></Circle>
+          <Circle></Circle>  
         </Name>
+       <Resume>Resume</Resume> 
       </Landing>
       
     </>
