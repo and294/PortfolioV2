295910a5a698c08e8c91aaac7848379cd15026c1
Glitch (glitch-blank-react) <none>
2022-09-04 14:01:43 +0000

    🎏🎃 Checkpoint ./src/pages/About/index.jsx:32506642/419

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -7,25 +7,39 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styled from "styled-components";
 
+const AboutSection = styled.section`
+background: #0D0D0D;
+color: white;
+height: 100vh;
+display: flex;
+padding: 50px;
+`;
+
+const Left = styled.div`
+width: 30%;
+`;
+
+const Right = styled.div``;
+
 export default function WelcomeBand() {
   return (
     <>
-      <section>
-        <left>
-          <h1><span style={{color: ""}}>&#x2022;</span>&nbsp;About Me</h1>
+      <AboutSection>
+        <Left>
+          <h1 style={{ fontSize: "32px"}}><span style={{color: "#4c6096"}}>&#x2022;</span>&nbsp;About Me</h1>
           <p>
-            I am a self thaught front-end developer from Paris. With diplomas in
+            I am a <span  style={{color: "#4c6096"}}>self thaught front-end developer</span> from Paris. <br/><br/>With diplomas in
             international sales and marketing, I've spent the last few years in
             pharmaceutical sales. A global pandemic and a lockdown later, I've
-            came to realise that a career change was needed in my life. Falling
+            came to realise that a career change was needed in my life. <br/><br/>Falling
             in love with web development, I've dived head first in that new
             adventure. The rigour of my past experiences, my artistic eye, my
             attention to detail and my ability to adapt/learn quickly will allow
             me to be able to help you with every aspects of your projects.
           </p>
-        </left>
+        </Left>
         <right></right>
-      </section>
+      </AboutSection>
     </>
   );
 }
