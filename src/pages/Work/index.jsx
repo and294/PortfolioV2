21280242163125dc770aea13345abd7b078c8a29
Glitch (glitch-blank-react) <none>
2022-09-05 09:18:25 +0000

    💄🎷 Checkpoint ./src/pages/Work/index.jsx:32506642/1210

--- src/pages/Work/index.jsx
+++ src/pages/Work/index.jsx
@@ -10,12 +10,31 @@ import styled from "styled-components";
 const WorkSection = styled.section`
   background: #181818;
   color: white;
+  font-family: "GT america";
   width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 70px;
   padding-top: 125px;
+`;
+
+const Project = styled.div`
+display: flex;
+align-items: center;
+`;
+
+const StyledImg = styled.img`
+width: 55%;
+border-radius: 5px;
+`;
+
+const DescriptionRight = styled.div`
+margin-left: 150px;
+`;
+
+const DescriptionLeft = styled.div`
+
 `;
 
 export default function Work() {
@@ -36,12 +55,16 @@ export default function Work() {
           <span style={{ color: "#4c6096", fontSize: "35px" }}>&#x2022;</span>
         </h1>
         
-        <div>
-        <img src="url('https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/Screenshot%202022-05-05%20at%2011-48-47%20Sweet%20and%20Sour.png?v=1662368676833')"/>
-          <p></p>
-          <p></p>
-          <p></p>
-        </div>
+        <Project>
+        <StyledImg src={'https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/Screenshot%202022-05-05%20at%2011-48-47%20Sweet%20and%20Sour.png?v=1662368676833'}
+          width= '50%'
+          />
+          <DescriptionRight>
+          <p>Concept  restaurant</p>
+          <p>Personnal project</p>
+          <p>HTML CSS<br/>Javascript Gsap</p>
+          </DescriptionRight>         
+        </Project>
       </WorkSection>
     </>
   );
