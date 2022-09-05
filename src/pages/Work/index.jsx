d0f80f0c0ec2089184be1ab60e57cf5f039f0ec6
Glitch (glitch-blank-react) <none>
2022-09-05 09:42:24 +0000

    🐌🚧 Checkpoint ./src/pages/Work/index.jsx:32506642/2556 ./.glitch-assets:32506642/867

--- src/pages/Work/index.jsx
+++ src/pages/Work/index.jsx
@@ -19,9 +19,16 @@ const WorkSection = styled.section`
   padding-top: 125px;
 `;
 
-const Project = styled.div`
+const ProjectRight = styled.div`
 display: flex;
 align-items: center;
+justify-content: flex-start;
+`;
+
+const ProjectLeft = styled.div`
+display: flex;
+align-items: center;
+justify-content: flex-end;
 `;
 
 const StyledImg = styled.img`
@@ -55,7 +62,7 @@ export default function Work() {
           <span style={{ color: "#4c6096", fontSize: "35px" }}>&#x2022;</span>
         </h1>
         
-        <Project>
+        <ProjectRight>
         <StyledImg src={'https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/Screenshot%202022-05-05%20at%2011-48-47%20Sweet%20and%20Sour.png?v=1662368676833'}
           width= '50%'
           />
@@ -63,9 +70,30 @@ export default function Work() {
           <p>Concept  restaurant</p>
           <p style={{ width: "300px",borderTop: "solid 1px white", borderBottom: "solid 1px white", margin: "20px 0px 20px 0px", padding: "20px 0px 20px 0px"}}>Personnal project</p>
           <p>HTML CSS<br/>Javascript Gsap</p>
-            <button>See it live</button>
           </DescriptionRight>         
-        </Project>
+        </ProjectRight>
+        
+        <ProjectLeft style={{marginTop: "100px"}}>     
+          <DescriptionLeft>
+          <p>Freelance agency</p>
+          <p style={{ width: "300px",borderTop: "solid 1px white", borderBottom: "solid 1px white", margin: "20px 0px 20px 0px", padding: "20px 0px 20px 0px"}}>Personnal project</p>
+          <p>HTML CSS<br/>ReactJS Gsap</p>
+          </DescriptionLeft>
+          <StyledImg src={'https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/Miniature%20Shiny%20portfolio.png?v=1662370737822'}
+          width= '50%'
+          />
+        </ProjectLeft>
+        
+        <ProjectRight style={{marginTop: "100px"}}>
+        <StyledImg src={'https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/Group%201BB58%20landing.png?v=1662370725354'}
+          width= '50%'
+          />
+          <DescriptionRight>
+          <p>Concept  restaurant</p>
+          <p style={{ width: "300px",borderTop: "solid 1px white", borderBottom: "solid 1px white", margin: "20px 0px 20px 0px", padding: "20px 0px 20px 0px"}}>Personnal project</p>
+          <p>HTML CSS<br/>Javascript Gsap</p>
+          </DescriptionRight>         
+        </ProjectRight>
       </WorkSection>
     </>
   );
