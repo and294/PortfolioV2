30d31cc50282280112a2361214a5784e7bca982f
Glitch (glitch-blank-react) <none>
2022-09-07 08:36:59 +0000

    🏨🕶 Checkpoint ./src/pages/ToolboxBand/index.jsx:32506642/3366 ./src/app.jsx:32506642/61

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import WelcomeBand from "/src/pages/WelcomeBand";
 import About from "/src/pages/About";
 import Work from "/src/pages/Work";
+import WelcomeBand from "/src/pages/WelcomeBand";
 
 import "./styles/styles.css";
 
