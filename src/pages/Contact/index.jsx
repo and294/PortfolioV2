16d9125c13221e052b4006fbea2794b19264a51c
Glitch (glitch-blank-react) <none>
2022-09-07 10:35:25 +0000

    🚖👲 Checkpoint ./src/pages/Contact/index.jsx:32506642/1037 ./.glitch-assets:32506642/1130

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -14,11 +14,35 @@ const ContactSection = styled.section`
   display: flex;
   padding: 70px;
   padding-top: 125px;
-  
+  font-family: "GT america";
 `;
 
 const Left = styled.div`
 
+`;
+
+const Title = styled.h1`
+font-size: 3.5em;
+`;
+
+const Mail = styled.p`
+display: flex;
+align-items: center;
+`;
+
+const Phone = styled.p`
+display: flex;
+align-items: center;
+`;
+
+const MailImg = styled.img`
+width: 25%;
+margin-right: 20px;
+`;
+
+const PhoneImg = styled.img`
+width: 23%;
+margin-right: 15px;
 `;
 
 const Right = styled.div`
@@ -32,9 +56,9 @@ export default function Contact() {
     <>
       <ContactSection>
         <Left>
-          <h1>Contact Me</h1>
-          <p><img/>and29ferrar</p>
-          <p></p>
+          <Title>Contact Me</Title>
+          <Mail><MailImg src={"https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/mail.svg?v=1662546414864"}/>and29ferraro@gmail.com</Mail>
+          <Phone><PhoneImg src={"https://cdn.glitch.global/7812f7dc-b12e-4d13-ad62-9680c2811117/phone%20bg.svg?v=1662546558035"}/>+33 6 87 80 22 06</Phone>
         </Left>
         <Right>
           
