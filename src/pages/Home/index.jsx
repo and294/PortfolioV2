da560211d4246539d4d2ecf9a765399e4cd4d481
Glitch (square-marshy-flag) <none>
2022-09-08 10:28:52 +0000

    🌞🍳 Checkpoint ./src/styles/Device/index.jsx:32506642/866 ./src/pages/Home/index.jsx:32506642/14550 ./src/pages/Navbar/index.jsx:32506642/843

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,6 +8,25 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styled from "styled-components";
 import Navbar from "/src/pages/Navbar";
 
+export const sizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "428px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+export const device = {
+  mobileS: `(min-width: ${sizes.mobileS})`,
+  mobileM: `(min-width: ${sizes.mobileM})`,
+  mobileL: `(min-width: ${sizes.mobileL})`,
+  tablet: `(min-width: ${sizes.tablet})`,
+  laptop: `(min-width: ${sizes.laptop})`,
+  laptopL: `(min-width: ${sizes.laptopL})`,
+  desktop: `(min-width: ${sizes.desktop})`,
+};
+
 gsap.registerPlugin(ScrollTrigger);
 
 const Landing = styled.section`
@@ -22,12 +41,89 @@ const Landing = styled.section`
   position: relative;
 `;
 
-const Name = styled.div`
+const NameSection = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   position: relative;
+
+  @media ${device.mobileS} {
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+  }
+  @media ${device.laptop} {
+  } ;
+`;
+
+const Name = styled.h1`
+  font-size: 126px;
+  font-family: "SaintMonica";
+  z-index: 10;
+  text-align: center;
+  @media ${device.mobileS} {
+    font-size: 36px;
+  }
+  @media ${device.mobileM} {
+    font-size: 40px;
+  }
+  @media ${device.tablet} {
+    font-size: 90px;
+  }
+  @media ${device.laptop} {
+    font-size: 126px;
+  } ;
+`;
+
+const Frontend = styled.h2`
+  font-family: "GT america";
+  z-index: 10;
+  @media ${device.mobileS} {
+    font-size: 14px;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+    font-size: 20px;
+  }
+  @media ${device.laptop} {
+    font-size: 34px;
+  } ;
+`;
+
+const Line = styled.span`
+  height: 2px;
+  background-color: rgb(76, 96, 150);
+  margin: 10px;
+  @media ${device.mobileS} {
+    width: 130px;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+    width: 150px;
+  }
+  @media ${device.laptop} {
+    width: 180px;
+  } ;
+`;
+
+const Paris = styled.p`
+  z-index: 10;
+  font-family: "GT america";
+  @media ${device.mobileS} {
+    font-size: 10px;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+    font-size: 12px;
+  }
+  @media ${device.laptop} {
+    font-size: 14px;
+  } ;
 `;
 
 const Circle = styled.span`
@@ -39,69 +135,96 @@ const Circle = styled.span`
   z-index: 0;
   -webkit-box-shadow: 0px 0px 21px 10px #4c6096;
   box-shadow: 0px 0px 21px 10px #4c6096;
+  @media ${device.mobileS} {
+    width: 300px;
+    height: 300px;
+    border: solid #4c6096 5px;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+    width: 400px;
+    height: 400px;
+    border: solid #4c6096 7px;
+  }
+  @media ${device.laptop} {
+    width: 500px;
+    height: 500px;
+    border: solid #4c6096 10px;
+  } ;
 `;
 
 const Resume = styled.button`
-  width: 100px;
-  height: 50px;
   color: white;
   font-family: GT america;
   background-color: #4c6096;
   border: none;
-  border-radius: 25px;
-  margin: 50px;
   position: absolute;
   bottom: 0;
   left: 0;
+  @media ${device.mobileS} {
+    font-size: 0.75em;
+    margin: 20px;
+    width: 75px;
+    height: 40px;
+    border-radius: 20px;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+  }
+  @media ${device.laptop} {
+    font-size: 1em;
+    margin: 50px;
+    width: 100px;
+    height: 50px;
+    border-radius: 25px;
+  } ;
 `;
 
 const Scroll = styled.p`
   position: absolute;
   bottom: 0;
   right: 0;
-  margin: 50px;
-  padding: 10px;
-  height: 80px;
+
   font-family: GT america;
   border-left: solid #4c6096;
   display: flex;
   align-items: center;
+  @media ${device.mobileS} {
+    font-size: 0.75em;
+    margin: 20px;
+    padding: 10px;
+    height: 50px;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.tablet} {
+  }
+  @media ${device.laptop} {
+    font-size: 1em;
+    margin: 50px;
+    padding: 10px;
+    height: 80px;
+  } ;
 `;
 
-
 export default function Home() {
-
   return (
     <>
       <Landing>
         <Navbar />
-        <Name>
-          <h1
-            className="title"
-            style={{
-              fontSize: "126px",
-              fontFamily: "SaintMonica",
-              zIndex: "10",
-            }}
+        <NameSection>
+          <Name>ANDREA FERRARO</Name>
+          <Frontend>Frontend Developer</Frontend>
+          <Line></Line>
+          <Paris
+            style={{ fontSize: "14px", zIndex: "10", fontFamily: "GT america" }}
           >
-            ANDREA FERRARO
-          </h1>
-          <h2
-            style={{ fontSize: "34px", fontFamily: "GT america", zIndex: "10" }}
-          >
-            Frontend Developer
-          </h2>
-          <span
-            style={{
-              width: "180px",
-              height: "2px",
-              backgroundColor: "rgb(76, 96, 150)",
-              margin: "10px",
-            }}
-          ></span>
-          <p style={{ fontSize: "14px", zIndex: "10", fontFamily: "GT america" }}>Paris, France</p>
+            Paris, France
+          </Paris>
           <Circle></Circle>
-        </Name>
+        </NameSection>
         <Resume>Resume</Resume>
         <Scroll>Scroll</Scroll>
       </Landing>
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -4,14 +4,40 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import styled from "styled-components";
 
+export const sizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "428px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+export const device = {
+  mobileS: `(min-width: ${sizes.mobileS})`,
+  mobileM: `(min-width: ${sizes.mobileM})`,
+  mobileL: `(min-width: ${sizes.mobileL})`,
+  tablet: `(min-width: ${sizes.tablet})`,
+  laptop: `(min-width: ${sizes.laptop})`,
+  laptopL: `(min-width: ${sizes.laptopL})`,
+  desktop: `(min-width: ${sizes.desktop})`,
+};
+
 const Nav = styled.nav`
-display: flex;
-justify-content: flex-end;
-padding: 40px;
-position: absolute;
-top: 0;
-right: 0;
+  display: flex;
+  justify-content: flex-end;
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  @media ${device.mobileS} {
+    padding: 20px;
+  }
+  @media ${device.laptop} {
+    padding: 40px;
+  } ;
 `;
+
 
 const NavLink = styled(Link)`
 
