cea2d536b52ce01f6cc78032195f54fb1f3728c3
Glitch (glitch-blank-react) <none>
2022-09-08 09:42:16 +0000

    🏩🌝 Checkpoint ./src/pages/Home/index.jsx:32506642/9396

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -64,62 +64,67 @@ const Name = styled.h1`
   z-index: 10;
   text-align: center;
   @media ${device.mobileS} {
-  font-size: 36px;
+    font-size: 36px;
   }
   @media ${device.mobileM} {
-
+    font-size: 40px;
   }
   @media ${device.tablet} {
-  font-size: 90px;
+    font-size: 90px;
   }
   @media ${device.laptop} {
-  font-size: 126px;
+    font-size: 126px;
   } ;
 `;
 
 const Frontend = styled.h2`
-  font-size: 34px;
+  
   font-family: "GT america";
   z-index: 10;
   @media ${device.mobileS} {
-  font-size: 14px;
+    font-size: 14px;
   }
   @media ${device.mobileM} {
   }
   @media ${device.tablet} {
+    font-size: 20px;  
   }
   @media ${device.laptop} {
+  font-size: 34px;
   } ;
 `;
 
 const Line = styled.span`
-  width: 180px;
   height: 2px;
   background-color: rgb(76, 96, 150);
   margin: 10px;
   @media ${device.mobileS} {
-  width: 130px;
+    width: 130px;
   }
   @media ${device.mobileM} {
   }
   @media ${device.tablet} {
+     width: 150px; 
   }
   @media ${device.laptop} {
+  width: 180px;
+  
   } ;
 `;
 
-const Paris = styled.p`
-  font-size: 14px;
+const Paris = styled.p` 
   z-index: 10;
   font-family: "GT america";
   @media ${device.mobileS} {
-  font-size: 10px;
+    font-size: 10px;
   }
   @media ${device.mobileM} {
   }
   @media ${device.tablet} {
+  font-size: 12px;
   }
   @media ${device.laptop} {
+  font-size: 14px;
   } ;
 `;
 
@@ -133,34 +138,49 @@ const Circle = styled.span`
   -webkit-box-shadow: 0px 0px 21px 10px #4c6096;
   box-shadow: 0px 0px 21px 10px #4c6096;
   @media ${device.mobileS} {
+    width: 300px;
+    height: 300px;
+    border: solid #4c6096 5px;
   }
   @media ${device.mobileM} {
   }
   @media ${device.tablet} {
+  width: 400px;
+  height: 400px;
+  border: solid #4c6096 7px;
   }
   @media ${device.laptop} {
+  width: 500px;
+  height: 500px;
+  border: solid #4c6096 10px;
   } ;
 `;
 
-const Resume = styled.button`
-  width: 100px;
-  height: 50px;
+const Resume = styled.button`  
   color: white;
   font-family: GT america;
   background-color: #4c6096;
   border: none;
-  border-radius: 25px;
-  margin: 50px;
   position: absolute;
   bottom: 0;
   left: 0;
   @media ${device.mobileS} {
+  font-size: 0.75em;
+  margin: 20px;
+  width: 75px;
+  height: 40px;
+  border-radius: 20px;
   }
   @media ${device.mobileM} {
   }
   @media ${device.tablet} {
   }
   @media ${device.laptop} {
+  font-size: 1em;
+  margin: 50px;
+  width: 100px;
+  height: 50px;
+  border-radius: 25px;
   } ;
 `;
 
@@ -168,20 +188,24 @@ const Scroll = styled.p`
   position: absolute;
   bottom: 0;
   right: 0;
-  margin: 50px;
-  padding: 10px;
-  height: 80px;
+  
   font-family: GT america;
   border-left: solid #4c6096;
   display: flex;
   align-items: center;
   @media ${device.mobileS} {
+   margin: 50px;
+  padding: 10px;
+  height: 50px;
   }
   @media ${device.mobileM} {
   }
   @media ${device.tablet} {
   }
   @media ${device.laptop} {
+  margin: 50px;
+  padding: 10px;
+  height: 80px;
   } ;
 `;
 
